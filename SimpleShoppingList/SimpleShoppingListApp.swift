a8057fe52ee8b6a6d8506351b878db8cd16c9652
David Thorn <david.thorn221278@googlemail.com>
2026-02-14 10:24:14 +0100

    Initial Commit

---
+++ SimpleShoppingList/SimpleShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleShoppingListApp.swift
+//  SimpleShoppingList
+//
+//  Created by David Thorn on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
